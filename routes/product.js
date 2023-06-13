f0b1183a26961e797e81800b60ea3952efc8a626
Tanay <daveth1106@gmail.com>
2023-06-13 12:37:46 +0530

    M-V-C applied and used router

---
+++ routes/product.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const productController = require('../controllers/product')
+
+
+router
+ .post("/", productController.createProduct)
+ .get("/", productController.getAllProducts)
+ .get("/:id", productController.getProduct)
+ .put("/:id", productController.replaceProduct)
+ .patch("/:id", productController.updateProduct)
+ .delete("/:id", productController.deleteProduct)
+
+ exports.router= router
